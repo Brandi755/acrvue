2085d7a5f23db55f561e9dc763e73156c9e587a1
Brandi755 <69139713+Brandi755@users.noreply.github.com>
2021-05-21 19:56:02 +0200

    push heroku build

---
+++ dist/precache-manifest.2b6c0bc037c09d72f9ccd56395c33315.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef464a9e0563b4f8005a",
+    "url": "/css/app.42740e02.css"
+  },
+  {
+    "revision": "0a4b4abb878ae70a9679",
+    "url": "/css/chunk-95d1bbf6.4f2e3611.css"
+  },
+  {
+    "revision": "beff89a2e1319d3ab362",
+    "url": "/css/chunk-vendors.650e1e09.css"
+  },
+  {
+    "revision": "048c44eb94bd874b473c12b06dadcbe8",
+    "url": "/img/b13.048c44eb.jpeg"
+  },
+  {
+    "revision": "9a0e69f4822f759048bfc65f65340e0d",
+    "url": "/img/bjpi.9a0e69f4.jpeg"
+  },
+  {
+    "revision": "950feef5f0f8a44abe86d01a45ebb6a5",
+    "url": "/img/chevrolet.950feef5.jpeg"
+  },
+  {
+    "revision": "09f7733ae64bba847e5e6a96215e0c0d",
+    "url": "/img/cle.09f7733a.png"
+  },
+  {
+    "revision": "44ce502b25a71078e4a04b2d417e0da1",
+    "url": "/img/clio.44ce502b.jpeg"
+  },
+  {
+    "revision": "018a1de08a6828959628492d078ce966",
+    "url": "/img/dacia.018a1de0.jpeg"
+  },
+  {
+    "revision": "2831761f73aa47291df79b5f5c94ce49",
+    "url": "/img/espace.2831761f.jpeg"
+  },
+  {
+    "revision": "babb4f30c464a6b810aa260d23a1e3f3",
+    "url": "/img/fiat.babb4f30.jpeg"
+  },
+  {
+    "revision": "9a0e69f4822f759048bfc65f65340e0d",
+    "url": "/img/golf.9a0e69f4.jpeg"
+  },
+  {
+    "revision": "0152d9472418fc5f20a6354b14eb4fd7",
+    "url": "/img/koleos.0152d947.jpeg"
+  },
+  {
+    "revision": "4982804767afd06a269d2374760b9d4f",
+    "url": "/img/logost.49828047.png"
+  },
+  {
+    "revision": "224735dc5fa1c408e8f6d76515501744",
+    "url": "/img/opnr.224735dc.jpeg"
+  },
+  {
+    "revision": "7ee0cea70e8a212ef7a08c786a41abae",
+    "url": "/img/talisman.7ee0cea7.jpeg"
+  },
+  {
+    "revision": "934ff31f4b35e7d1ea4a36256b5ac2ef",
+    "url": "/img/telecommande.934ff31f.jpeg"
+  },
+  {
+    "revision": "c2b4afd7746f1320a24b02f4de101ea6",
+    "url": "/img/trafic.c2b4afd7.jpeg"
+  },
+  {
+    "revision": "92c93c7667124a4970c6493c1594ca14",
+    "url": "/img/velsatis.92c93c76.jpeg"
+  },
+  {
+    "revision": "275112abc2e0012f2ddf6d4c7ed4c751",
+    "url": "/img/voiture.275112ab.jpeg"
+  },
+  {
+    "revision": "4b8a9c5959974ae8e27c0c91a38a06aa",
+    "url": "/img/voiture.4b8a9c59.png"
+  },
+  {
+    "revision": "a921b882675a16c43a98564eca694386",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef464a9e0563b4f8005a",
+    "url": "/js/app.a1fbbd25.js"
+  },
+  {
+    "revision": "0a4b4abb878ae70a9679",
+    "url": "/js/chunk-95d1bbf6.ddd05953.js"
+  },
+  {
+    "revision": "beff89a2e1319d3ab362",
+    "url": "/js/chunk-vendors.a968fff0.js"
+  },
+  {
+    "revision": "54a86dcd6a69292eed27ec37dcd07c66",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
